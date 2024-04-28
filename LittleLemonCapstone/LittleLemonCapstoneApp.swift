1eb15b1578f40818d03c24a5ed4d06543f912491
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-04-28 21:26:47 +0300

    Initial Commit

---
+++ LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonCapstoneApp.swift
+//  LittleLemonCapstone
+//
+//  Created by Wijdan Almutairi on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonCapstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
