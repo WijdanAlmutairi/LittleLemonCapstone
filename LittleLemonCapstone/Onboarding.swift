38158c3942a17d41cec186e7a786d002d78cfe37
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-05-17 19:31:48 +0300

    style(ui): enhance onboarding screen with text field labels, scroll view, and focus
    - Added text field labels to provide clarity for user input on the onboarding screen
    - Implemented a scroll view to allow users to navigate through the content seamlessly
    - Enabled focusing on text fields for improved user interaction and accessibility

--- LittleLemonCapstone/Onboarding.swift
+++ LittleLemonCapstone/Onboarding.swift
@@ -12,6 +12,9 @@ let kLastName = "last name key"
 let kEmail = "email key"
 let kIsLoggedIn = "kIsLoggedIn"
 
+enum FocusableField: Hashable {
+    case firstName, lastName, email
+}
 
 struct Onboarding: View {
     @State private var firstName = ""
@@ -19,61 +22,85 @@ struct Onboarding: View {
     @State private var email = ""
     
     @State private var isLoggedIn = false
-
-
+    
+    @FocusState private var focusedField: FocusableField?
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Image("Logo")
-                Spacer()
-                HeroSection()
-                NavigationLink(destination: Home(), isActive: $isLoggedIn){
-                                EmptyView()
-                            }
-                TextField(" First Name", text: $firstName)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                TextField(" Last Name", text: $lastName)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-
-                TextField(" Email", text: $email)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Spacer()
-                Button(action: {
-                    if(!firstName.isEmpty && !lastName.isEmpty && !email.isEmpty) {
-                        UserDefaults.standard.set(firstName, forKey: kFirstName)
-                        UserDefaults.standard.set(lastName, forKey: kLastName)
-                        UserDefaults.standard.set(email, forKey: kEmail)
-                        isLoggedIn = true
-                        UserDefaults.standard.set(isLoggedIn, forKey: kIsLoggedIn)
-                        
-                    }else {
-                        print ("first , last and email is empty")
+            ScrollView {
+                VStack {
+                    Image("Logo")
+                    Spacer()
+                    HeroSection()
+                        .padding(.bottom, 20)
+                    NavigationLink(destination: Home(), isActive: $isLoggedIn){
+                        EmptyView()
                     }
-                    
-                    
-                }){
-                    Text("Register")
-                        .foregroundColor(Color("AccentColor"))
-                        .padding()
-                        .bold()
-                        .padding(.trailing, 110)
-                        .padding(.leading, 110)
+                    VStack {
+                        Text("First Name*")
+                            .foregroundColor(.gray)
+                            .frame(maxWidth:.infinity,alignment: .leading)
                         
-                                        .background(Color("PrimaryOne"))
-                                        .cornerRadius(10)
+                        TextField(" First Name", text: $firstName)
+                            .focused($focusedField, equals: .firstName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }.padding(10)
+                    
+                    VStack {
+                        Text("Last Name*")
+                            .foregroundColor(.gray)
+                            .frame(maxWidth:.infinity,alignment: .leading)
+                        
+                        TextField(" Last Name", text: $lastName)
+                            .keyboardType(.emailAddress)
+                            .focused($focusedField, equals: .lastName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }.padding(10)
+                    
+                    
+                    VStack {
+                        Text("Email*")
+                            .foregroundColor(.gray)
+                            .frame(maxWidth:.infinity,alignment: .leading)
+                        
+                        TextField(" Email", text: $email)
+                            .focused($focusedField, equals: .email)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }.padding(10)
+                    Spacer()
+                    Button(action: {
+                        if(!firstName.isEmpty && !lastName.isEmpty && !email.isEmpty) {
+                            UserDefaults.standard.set(firstName, forKey: kFirstName)
+                            UserDefaults.standard.set(lastName, forKey: kLastName)
+                            UserDefaults.standard.set(email, forKey: kEmail)
+                            isLoggedIn = true
+                            UserDefaults.standard.set(isLoggedIn, forKey: kIsLoggedIn)
+                            
+                        }else {
+                            print ("first , last and email is empty")
+                        }
+                        
+                        
+                    }){
+                        Text("Register")
+                            .foregroundColor(Color("AccentColor"))
+                            .padding()
+                            .bold()
+                            .padding(.trailing, 110)
+                            .padding(.leading, 110)
+                        
+                            .background(Color("PrimaryOne"))
+                            .cornerRadius(10)
+                    }.padding(.top, 30)
+                    Spacer()
                 }
-                Spacer()
             }
-           
+            
         }.onAppear(){
             if (UserDefaults.standard.bool(forKey: kIsLoggedIn)){
                 isLoggedIn = true
             }
+            focusedField = .firstName
         }
     }
 }
